dcfbd5f0a83953a459c32dfaf0407003c808889f
Camille <camille.morand-duval@student-cs.fr>
2020-03-25 22:17:48 +0000

    Containing all function + Weird y-test to test code

--- template_script_V2_AM.py
+++ template_script_V2_AM.py
@@ -19,6 +19,8 @@ import numpy as np
 import pandas as pd
 import spacy
 import fr_core_news_sm
+import en_core_web_sm
+import de_core_news_sm
 
 # Choose number of samples tested
 import math
@@ -52,6 +54,11 @@ from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import validation_curve
 from sklearn.model_selection import RandomizedSearchCV
 
+from sklearn.metrics import f1_score
+from sklearn.metrics import accuracy_score
+
+
+import re 
 """
     ii) Load spaCy for french.
 """
@@ -63,13 +70,153 @@ spacy_nlp = fr_core_news_sm.load()
 """
 
 X_train = pd.read_csv('X_train.csv')
-Y_train = pd.read_csv('Y_train.csv')
+y_train = pd.read_csv('Y_train.csv')
 X_test = pd.read_csv('X_test.csv')
+y_test = y_train[:13812]
 
 """
     Pre-processing
 """
+# designation and ids
+def cleaning(X_train): 
+    X_train = X_train.drop(['description', 'productid','imageid'], axis=1)
+    X_train.columns = ['integer_id', 'designation']
+    return X_train
 
+def normalize_accent(string):
+    string = string.replace('á', 'a')
+    string = string.replace('â', 'a')
+
+    string = string.replace('é', 'e')
+    string = string.replace('è', 'e')
+    string = string.replace('ê', 'e')
+    string = string.replace('ë', 'e')
+
+    string = string.replace('î', 'i')
+    string = string.replace('ï', 'i')
+
+    string = string.replace('ö', 'o')
+    string = string.replace('ô', 'o')
+    string = string.replace('ò', 'o')
+    string = string.replace('ó', 'o')
+
+    string = string.replace('ù', 'u')
+    string = string.replace('û', 'u')
+    string = string.replace('ü', 'u')
+
+    string = string.replace('ç', 'c')
+    
+    return string
+ 
+def del_digits(list): 
+    pattern = '[0-9]'
+    list = [re.sub(pattern, '', i) for i in list] 
+    return list
+
+def del_date(list): 
+    pattern = '^(?:(?:[0-9]{2}[:\/,]){2}[0-9]{2,4}|am|pm)$'
+    list = [re.sub(pattern, '', i) for i in list] 
+    return list
+
+def del_specialchar(list): 
+    pattern = '\W'
+    list = [re.sub(pattern, '', i) for i in list] 
+    return list
+
+def del_letters(list): 
+    for i in list:
+      if len(i) <= 1:
+        list.remove(i)
+    return list
+
+def raw_to_tokens(raw_string, spacy_nlp):
+    # Write code for lower-casing
+    string = raw_string.lower()
+    
+    # Write code to normalize the accents
+    string = normalize_accent(string)
+        
+    # Write code to tokenize
+    spacy_tokens = spacy_nlp(string)
+        
+    # Write code to remove punctuation tokens and create string tokens
+    string_tokens = [token.orth_ for token in spacy_tokens if not token.is_punct if not token.is_stop]
+
+    # Write code to remove digits
+    string_tokens = del_digits(string_tokens)
+
+    # Write code to remove special character
+    string_tokens = del_specialchar(string_tokens)
+
+    # Write code to remove dates
+    string_tokens = del_date(string_tokens)
+
+    # Write code to remove single letters
+    string_tokens = del_letters(string_tokens)
+    
+    # Write code to join the tokens back into a single string
+    clean_string = " ".join(string_tokens)
+
+    #return clean_string
+    return clean_string
+
+## Apply pre-processing
+
+# X_train - step takes roughly ~15:30 min 
+X_train = cleaning(X_train)
+spacy_nlp = fr_core_news_sm.load() 
+X_train['designation_cleaned'] = X_train['designation'].apply(lambda x: raw_to_tokens(x, spacy_nlp))
+spacy_nlp = spacy.load('en_core_web_sm')
+X_train['designation_cleaned'] = X_train['designation_cleaned'].apply(lambda x: raw_to_tokens(x, spacy_nlp))
+spacy_nlp = de_core_news_sm.load()
+X_train['designation_cleaned'] = X_train['designation_cleaned'].apply(lambda x: raw_to_tokens(x, spacy_nlp))
+
+# X_test - step takes roughly ~2:20 min 
+X_test = cleaning(X_test)
+
+spacy_nlp = fr_core_news_sm.load()
+X_test['designation_cleaned'] = X_test['designation'].apply(lambda x: raw_to_tokens(x, spacy_nlp))
+spacy_nlp = spacy.load('en_core_web_sm')
+X_test['designation_cleaned'] = X_test['designation_cleaned'].apply(lambda x: raw_to_tokens(x, spacy_nlp))
+spacy_nlp = de_core_news_sm.load()
+X_test['designation_cleaned'] = X_test['designation_cleaned'].apply(lambda x: raw_to_tokens(x, spacy_nlp))
+
+# create a list from the processed cells
+doc_clean_train =  X_train['designation_cleaned'].astype('U').tolist()
+doc_clean_test = X_test['designation_cleaned'].astype('U').tolist()
+#doc_clean = doc_clean_train + doc_clean_test
+doc_clean = doc_clean_train
+doc_clean_test = doc_clean_train + doc_clean_test
+
+# import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# X-train
+# convert raw documents into TF-IDF matrix.
+tfidf = TfidfVectorizer()
+X_tfidf = tfidf.fit_transform(doc_clean)
+
+print("Shape of the TF-IDF Train Matrix:")
+print(X_tfidf.shape)
+
+# if no transformation is applied i.e. no PCA / truncated SVD: 
+X_transformed = X_tfidf
+X_train_T = X_transformed[:84916]
+print(X_train_T.shape) # 84916
+
+# X-test
+# convert raw documents into TF-IDF matrix.
+
+tfidf = TfidfVectorizer()
+X_tfidf = tfidf.fit_transform(doc_clean_test)
+
+print("Shape of the TF-IDF Test Matrix:")
+print(X_tfidf.shape)
+
+# if no transformation is applied i.e. no PCA / truncated SVD: 
+X_transformed = X_tfidf
+X_test_T = X_transformed[84916:]
+print(X_test_T.shape) # 13812
 
 """
     Your methods implementing the models.
@@ -84,14 +231,16 @@ X_test = pd.read_csv('X_test.csv')
         
     Following is a sample method. Please note that the parameters given here are just examples.
 """
-def model_random_forest(X_train, y_train, X_test, y_test):
+def model_decision_classifier(X_train, y_train, X_test, y_test):
     """
     @param: X_train - a numpy matrix containing features for training data (e.g. TF-IDF matrix)
     @param: y_train - a numpy array containing labels for each training sample
     @param: X_test - a numpy matrix containing features for test data (e.g. TF-IDF matrix)
     @param: y_test - a numpy array containing labels for each test sample
     """
-    clf = RandomForestClassifier(n_estimators = 100, max_depth=2, random_state=0) # please choose all necessary parameters
+    clf = DecisionTreeClassifier(criterion="gini", 
+                                 max_depth=180, 
+                                 splitter = "random") # please choose all necessary parameters
     clf.fit(X_train, y_train)
 
     y_predicted = clf.predict(X_test)
@@ -100,7 +249,126 @@ def model_random_forest(X_train, y_train, X_test, y_test):
 
     return rf_accuracy, rf_f1
 
+def model_random_forest(X_train, y_train, X_test, y_test):
+    """
+    @param: X_train - a numpy matrix containing features for training data (e.g. TF-IDF matrix)
+    @param: y_train - a numpy array containing labels for each training sample
+    @param: X_test - a numpy matrix containing features for test data (e.g. TF-IDF matrix)
+    @param: y_test - a numpy array containing labels for each test sample
+    """
+    clf = RandomForestClassifier(n_estimators = 1800, 
+                                 min_samples_split = 5, 
+                                 min_samples_leaf = 1, 
+                                 max_features = "auto", 
+                                 max_depth = None, 
+                                 bootstrap = True) # please choose all necessary parameters
+    clf.fit(X_train, y_train)
 
+    y_predicted = clf.predict(X_test)
+    rf_accuracy = accuracy_score(y_test, y_predicted)
+    rf_f1 = f1_score(y_test, y_predicted, average="weighted")
+
+    return rf_accuracy, rf_f1
+
+def model_boosting(X_train, y_train, X_test, y_test):
+    """
+    @param: X_train - a numpy matrix containing features for training data (e.g. TF-IDF matrix)
+    @param: y_train - a numpy array containing labels for each training sample
+    @param: X_test - a numpy matrix containing features for test data (e.g. TF-IDF matrix)
+    @param: y_test - a numpy array containing labels for each test sample
+    """
+    clf = xgb.XGBClassifier(tree_method = "approx", 
+                            subsample = 0.8, 
+                            silent = False,
+                            objective = "multi:softmax",
+                            num_classes = 27, 
+                            min_child_weight = 1, 
+                            max_depth = 10, 
+                            max_delta_step = 0,
+                            eta = 0.15,
+                            colsample_bytree = 0.8,
+                            colsample_bylevel = 1,
+                            booster = "gbtree") # please choose all necessary parameters
+    clf.fit(X_train, y_train)
+
+    y_predicted = clf.predict(X_test)
+    rf_accuracy = accuracy_score(y_test, y_predicted)
+    rf_f1 = f1_score(y_test, y_predicted, average="weighted")
+
+    return rf_accuracy, rf_f1
+
+def model_grad_boosted_trees(X_train, y_train, X_test, y_test):
+    """
+    @param: X_train - a numpy matrix containing features for training data (e.g. TF-IDF matrix)
+    @param: y_train - a numpy array containing labels for each training sample
+    @param: X_test - a numpy matrix containing features for test data (e.g. TF-IDF matrix)
+    @param: y_test - a numpy array containing labels for each test sample
+    """
+    clf = GradientBoostingClassifier(n_estimator = 30,
+                            max_depth = 50,
+                            learning_rate = 0.1) # please choose all necessary parameters
+    clf.fit(X_train, y_train)
+
+    y_predicted = clf.predict(X_test)
+    rf_accuracy = accuracy_score(y_test, y_predicted)
+    rf_f1 = f1_score(y_test, y_predicted, average="weighted")
+
+    return rf_accuracy, rf_f1
+
+def model_bagging(X_train, y_train, X_test, y_test):
+    """
+    @param: X_train - a numpy matrix containing features for training data (e.g. TF-IDF matrix)
+    @param: y_train - a numpy array containing labels for each training sample
+    @param: X_test - a numpy matrix containing features for test data (e.g. TF-IDF matrix)
+    @param: y_test - a numpy array containing labels for each test sample
+    """
+    
+    parameters = {"base_estimator__criterion" : ["gini", "entropy"],
+                  "base_estimator__splitter" :   ["best", "random"],
+                  "n_estimators": [10, 20, 30], 
+                  "max_samples" : [0.7, 0.8, 1]
+                  }
+
+    DTC = DecisionTreeClassifier(random_state = 11, 
+                             max_features = 0.8,
+                             max_depth = 6)
+
+    clf = BaggingClassifier(base_estimator = DTC) # please choose all necessary parameters
+    clf.fit(X_train, y_train)
+
+    y_predicted = clf.predict(X_test)
+    rf_accuracy = accuracy_score(y_test, y_predicted)
+    rf_f1 = f1_score(y_test, y_predicted, average="weighted")
+
+    return rf_accuracy, rf_f1
+
+def model_adaboost(X_train, y_train, X_test, y_test):
+    """
+    @param: X_train - a numpy matrix containing features for training data (e.g. TF-IDF matrix)
+    @param: y_train - a numpy array containing labels for each training sample
+    @param: X_test - a numpy matrix containing features for test data (e.g. TF-IDF matrix)
+    @param: y_test - a numpy array containing labels for each test sample
+    """
+    
+    parameters = {"n_estimators" : 100, 
+                  "learning_rate" : 0.1}
+
+    RFC = RandomForestClassifier(criterion = 'gini', 
+                             n_estimators = 2000, 
+                             min_samples_split = 10, 
+                             min_samples_leaf = 2, 
+                             max_features = 'sqrt', 
+                             max_depth = None, 
+                             bootstrap = True)
+
+    clf = BaggingClassifier(base_estimator = RFC) # please choose all necessary parameters
+    clf.fit(X_train, y_train)
+
+    y_predicted = clf.predict(X_test)
+    rf_accuracy = accuracy_score(y_test, y_predicted)
+    rf_f1 = f1_score(y_test, y_predicted, average="weighted")
+
+    return rf_accuracy, rf_f1
 """
    The main function should print all the accuracies and F1 scores for all the models.
    
@@ -108,19 +376,31 @@ def model_random_forest(X_train, y_train, X_test, y_test):
    
    Please make sure that your code is outputting the performances in proper format, because your script will be run automatically by a meta-script.
 """
+
 if __name__ == "__main__":
     """
-       This is just an example, plese change as necessary. Just maintain final output format with proper names of the models as described above.
+       This is just an example, please change as necessary. Just maintain final output format with proper names of the models as described above.
     """
-    model_1_acc, model_1_f1 = run_model_1(...)
-    model_2_acc, model_2_f1 = run_model_2(...)
+    model_1_acc, model_1_f1 = model_decision_classifier(X_train, y_train, X_test, y_test)
+    model_2_acc, model_2_f1 = model_random_forest(X_train, y_train, X_test, y_test)
+    model_3_acc, model_3_f1 = model_boosting(X_train, y_train, X_test, y_test)
+    model_4_acc, model_4_f1 = model_grad_boosted_trees(X_train, y_train, X_test, y_test)
+    model_5_acc, model_5_f1 = model_bagging(X_train, y_train, X_test, y_test)
+    model_6_acc, model_6_f1 = model_adaboost(X_train, y_train, X_test, y_test)
+        
+        
+    #model_1_acc, model_1_f1 = run_model_1(...)
     """
         etc.
     """
 
     # print the results
-    print("model_1", model_1_acc, model_1_f1)
-    print("model_2", model_2_acc, model_2_f1)
+    print("DecisionTreeClassifier", model_1_acc, model_1_f1)
+    print("RandomForestClassifier", model_2_acc, model_2_f1)
+    print("XGBClassifier", model_2_acc, model_2_f1)
+    print("GradientBoostingClassifier", model_2_acc, model_2_f1)
+    print("BaggingClassifier", model_2_acc, model_2_f1)
+    print("AdaBoostClassifier", model_2_acc, model_2_f1)
     """
         etc.
     """
